41a5c5f75cb40d9563d1231fe5a4129710d2b990
relaxedcoder <devjit.menghani@gmail.com>
2020-06-25 00:30:20 +0530

    Webhook callback URL

--- app.js
+++ app.js
@@ -16,6 +16,25 @@ setInterval(() => {
     db.notifyRestock()
 }, 5000)
 
+app.get("/webhook", (req, res) => {
+    // Parse the query params
+    let mode = req.query["hub.mode"]
+    let token = req.query["hub.verify_token"]
+    let challenge = req.query["hub.challenge"]
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+        // Checks the mode and token sent is correct
+        if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+            // Responds with the challenge token from the request
+            res.status(200).send(challenge)
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403)
+        }
+    }
+})
+
 app.post('/webhook', (req, res) => {
     var { body } = req
 
